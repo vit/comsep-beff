6ab02eac23d295e1296a2a1f52c16f4999eb93fc
vit <shiegin@gmail.com>
2019-11-17 22:32:24 +0000

    m

--- services/rpc/index.js
+++ services/rpc/index.js
@@ -1,18 +1,62 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+//module.exports = function (fastify, opts) {
 
-//  fastify.post('/rpc/rpc', {
   fastify.post('/rpc', {
     preValidation: [fastify.authenticate]
-  }, async function (request, reply) {
+  }, function (request, reply) {
+//  }, function (request, reply) {
     const models = this.mongoose;
     const {user, body} = request;
     const user_id = user && user.user ? user.user.id : null;
+
+//    const user_rec = user_id ? (await models.User.findById(user_id, '-encrypted_password')) : null;
+    models.User.findById(user_id, '-encrypted_password', function (err, user_rec) {
+      const ct = {models, user: user_rec};
+      const {model, proc, args} = request.body;
+  
+      if(
+        model &&
+        models &&
+        models[model] &&
+  //      (typeof models[model].rpc == 'function')
+        models[model].rpc &&
+        (typeof models[model].rpc[proc] == 'function')
+      ) {
+  //      models[model].rpc.call(models[model], {proc, args, cb: function(answer) {
+        models[model].rpc[proc].call(models[model], {ct, args, cb: function(answer, error) {
+  //        return {answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' }
+  //        reply.send({answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' })
+          reply.send({answer: answer, request: request.body, error })
+        }})
+      } else {
+        reply.send({answer: null, error: "rpc model not found" })
+      }
+    })
+
+//    reply.send({answer: null, error: "test" })
+
+  })
+
+}
+
+/*
+
+module.exports = async function (fastify, opts) {
+//module.exports = function (fastify, opts) {
+
+  fastify.post('/rpc', {
+    preValidation: [fastify.authenticate]
+  }, async function (request, reply) {
+//  }, function (request, reply) {
+    const models = this.mongoose;
+    const {user, body} = request;
+    const user_id = user && user.user ? user.user.id : null;
+
     const user_rec = user_id ? (await models.User.findById(user_id, '-encrypted_password')) : null;
     const ct = {models, user: user_rec};
     const {model, proc, args} = request.body;
-
 
 
     if(
@@ -25,14 +69,20 @@ module.exports = async function (fastify, opts) {
     ) {
 //      models[model].rpc.call(models[model], {proc, args, cb: function(answer) {
       models[model].rpc[proc].call(models[model], {ct, args, cb: function(answer, error) {
+//        return {answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' }
+//        reply.send({answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' })
         reply.send({answer: answer, request: request.body, error })
       }})
     } else {
       reply.send({answer: null, error: "rpc model not found" })
     }
 
+//    reply.send({answer: null, error: "test" })
+
   })
 
 }
+
+*/
 
 
