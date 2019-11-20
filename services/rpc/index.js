e60d1305a058c6fbc2e723540b68332457b638cb
vit <shiegin@gmail.com>
2019-11-20 17:17:13 +0000

    m

--- services/rpc/index.js
+++ services/rpc/index.js
@@ -13,23 +13,26 @@ module.exports = async function (fastify, opts) {
 
 //    const user_rec = user_id ? (await models.User.findById(user_id, '-encrypted_password')) : null;
     models.User.findById(user_id, '-encrypted_password', function (err, user_rec) {
-      const ct = {models, user: user_rec};
+      const ctx = {models, user: user_rec};
       const {model, proc, args} = request.body;
   
       if(
         model &&
         models &&
-        models[model] &&
-  //      (typeof models[model].rpc == 'function')
-        models[model].rpc &&
-        (typeof models[model].rpc[proc] == 'function')
+        models[model] //&&
+//        models[model].rpc &&
+//        (typeof models[model].rpc[proc] == 'function')
       ) {
-  //      models[model].rpc.call(models[model], {proc, args, cb: function(answer) {
-        models[model].rpc[proc].call(models[model], {ct, args, cb: function(answer, error) {
-  //        return {answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' }
-  //        reply.send({answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' })
-          reply.send({answer: answer, request: request.body, error })
-        }})
+        if (
+          models[model].rpc &&
+          (typeof models[model].rpc[proc] == 'function')
+        ) {
+          models[model].rpc[proc].call(models[model], {ctx, args, cb: function(answer, error) {
+            reply.send({answer: answer, request: request.body, error })
+          }})
+        } else {
+          reply.send({answer: null, error: "rpc function not found" })
+        }
       } else {
         reply.send({answer: null, error: "rpc model not found" })
       }
