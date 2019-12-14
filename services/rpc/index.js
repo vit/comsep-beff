ae5fcd43898363043d181c8b61472945145198dd
vit <shiegin@gmail.com>
2019-12-14 01:22:43 +0000

    m

--- services/rpc/index.js
+++ services/rpc/index.js
@@ -11,81 +11,40 @@ module.exports = async function (fastify, opts) {
     const {user, body} = request;
     const user_id = user && user.user ? user.user.id : null;
 
-//    const user_rec = user_id ? (await models.User.findById(user_id, '-encrypted_password')) : null;
     models.User.findById(user_id, '-encrypted_password', function (err, user_rec) {
       const ctx = {models, user: user_rec};
       const {model, proc, args} = request.body;
   
-      if(
-        model &&
-        models &&
-        models[model] //&&
-//        models[model].rpc &&
-//        (typeof models[model].rpc[proc] == 'function')
-      ) {
-        if (
-          models[model].rpc &&
-          (typeof models[model].rpc[proc] == 'function')
+//      console.log("/rpc/user_id:", user_id);
+//      console.log("/rpc/user_rec:", user_rec);
+
+      try {
+        if(
+          model &&
+          models &&
+          models[model]
         ) {
-          models[model].rpc[proc].call(models[model], {ctx, args, cb: function(answer, error) {
-            reply.send({answer: answer, request: request.body, error })
-          }})
+          if (
+            models[model].rpc &&
+            (typeof models[model].rpc[proc] == 'function')
+          ) {
+            models[model].rpc[proc].call(models[model], {ctx, args, cb: function(answer, error) {
+              reply.send({answer: answer, request: request.body, error })
+            }})
+          } else {
+            reply.send({answer: null, error: "rpc function not found" })
+          }
         } else {
-          reply.send({answer: null, error: "rpc function not found" })
+          reply.send({answer: null, error: "rpc model not found" })
         }
-      } else {
-        reply.send({answer: null, error: "rpc model not found" })
+      }
+      catch(error) {
+        console.log("/rpc/exception:", error);
+        reply.send({answer: null, error })
       }
     })
 
-//    reply.send({answer: null, error: "test" })
-
   })
 
 }
-
-/*
-
-module.exports = async function (fastify, opts) {
-//module.exports = function (fastify, opts) {
-
-  fastify.post('/rpc', {
-    preValidation: [fastify.authenticate]
-  }, async function (request, reply) {
-//  }, function (request, reply) {
-    const models = this.mongoose;
-    const {user, body} = request;
-    const user_id = user && user.user ? user.user.id : null;
-
-    const user_rec = user_id ? (await models.User.findById(user_id, '-encrypted_password')) : null;
-    const ct = {models, user: user_rec};
-    const {model, proc, args} = request.body;
-
-
-    if(
-      model &&
-      models &&
-      models[model] &&
-//      (typeof models[model].rpc == 'function')
-      models[model].rpc &&
-      (typeof models[model].rpc[proc] == 'function')
-    ) {
-//      models[model].rpc.call(models[model], {proc, args, cb: function(answer) {
-      models[model].rpc[proc].call(models[model], {ct, args, cb: function(answer, error) {
-//        return {answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' }
-//        reply.send({answer: 'aaa aaa aaa', request: 'qqq qqq qqq', error: 'eee eee eee' })
-        reply.send({answer: answer, request: request.body, error })
-      }})
-    } else {
-      reply.send({answer: null, error: "rpc model not found" })
-    }
-
-//    reply.send({answer: null, error: "test" })
-
-  })
-
-}
-
-*/
-
 
